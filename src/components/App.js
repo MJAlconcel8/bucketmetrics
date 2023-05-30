c0b1c187eded104d9d4d48c54e26776d5fc3f236
Mark Joseph Alconcel <109438699+MJAlconcel8@users.noreply.github.com>
2023-05-30 16:30:59 -0400

    Update App.js

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, {Component, components} from 'react'
+import {TopNavBar} from "./TopNavBar"
+import {Main} from "./Main"
+
+class App extends Component{
+  render(){
+    return(
+      <div className='App'>
+        <TopNavBar/>
+      </div>
+    );
+  }
+}
+export default App
